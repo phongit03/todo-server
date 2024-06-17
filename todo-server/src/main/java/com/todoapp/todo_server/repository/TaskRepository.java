340fe3330cd88e16e2046d89aa856d3195b8bb4e
phongit03 <phongtq003@gmail.com>
2024-06-17 22:43:44 +0700

    test

--- todo-server/src/main/java/com/todoapp/todo_server/repository/TaskRepository.java
+++ todo-server/src/main/java/com/todoapp/todo_server/repository/TaskRepository.java
@@ -4,7 +4,6 @@ import com.todoapp.todo_server.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
