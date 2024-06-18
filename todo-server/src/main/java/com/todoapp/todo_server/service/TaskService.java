cf36bb75f98b25bc99060bd294f288a2323b8cac
phongit03 <phongtq003@gmail.com>
2024-06-18 22:57:09 +0700

    Injected userRepo and added filter task by user service

--- todo-server/src/main/java/com/todoapp/todo_server/service/TaskService.java
+++ todo-server/src/main/java/com/todoapp/todo_server/service/TaskService.java
@@ -1,7 +1,9 @@
 package com.todoapp.todo_server.service;
 
 import com.todoapp.todo_server.entity.Task;
+import com.todoapp.todo_server.entity.UserEntity;
 import com.todoapp.todo_server.repository.TaskRepository;
+import com.todoapp.todo_server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,9 @@ import java.util.Optional;
 public class TaskService {
     @Autowired
     public TaskRepository taskRepository;
+
+    @Autowired
+    public UserRepository userRepository;
 
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
@@ -31,5 +36,10 @@ public class TaskService {
 
     public void deleteById(Long id) {
         taskRepository.deleteById(id);
+    }
+
+    public Optional<List<Task>> getTasksByUserId(Long userId) {
+        Optional<UserEntity> userAssigned = userRepository.findById(userId);
+        return taskRepository.findAllByUserAssigned(userAssigned);
     }
 }
