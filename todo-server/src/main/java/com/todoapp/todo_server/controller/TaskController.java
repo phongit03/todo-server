6219ae131e7f8ad3165510305a72096bb6e61b43
phongit03 <phongtq003@gmail.com>
2024-06-18 22:54:57 +0700

    Added endpoints add, assign and re-assign tasks to user

--- todo-server/src/main/java/com/todoapp/todo_server/controller/TaskController.java
+++ todo-server/src/main/java/com/todoapp/todo_server/controller/TaskController.java
@@ -2,6 +2,8 @@ package com.todoapp.todo_server.controller;
 
 import com.todoapp.todo_server.entity.Roles;
 import com.todoapp.todo_server.entity.Task;
+import com.todoapp.todo_server.entity.UserEntity;
+import com.todoapp.todo_server.repository.UserRepository;
 import com.todoapp.todo_server.service.TaskService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,8 +23,11 @@ public class TaskController {
     @Autowired
     public TaskService taskService;
 
+    @Autowired
+    public UserRepository userRepository;
+
     @GetMapping
-    public ResponseEntity<List<Task>> getAllTasks(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<List<Task>> getAllTasks() {
         try {
             List<Task> tasks = taskService.getAllTasks();
             if(tasks.isEmpty()) {
@@ -31,6 +38,17 @@ public class TaskController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<Optional<List<Task>>> getTasksAssignedToUser(@PathVariable Long userId) {
+        try {
+            Optional<List<Task>> tasks = taskService.getTasksByUserId(userId);
+            return new ResponseEntity<>(tasks, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{id}")
@@ -44,15 +62,36 @@ public class TaskController {
         }
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<Task> addTask(@RequestBody Task task) {
+    @PostMapping("/add/user/{userId}")
+    public ResponseEntity<Task> addTask(@RequestBody Task taskRequest, @PathVariable Long userId) {
         try {
-            Task newTask = taskService.addTask(new Task(task.getTitle(), task.getDescription()));
+            Task newTask = userRepository.findById(userId).map(user -> {
+               taskRequest.setUserAssigned(user);
+               return taskService.addTask(taskRequest);
+            }).orElseThrow();
+            System.out.println(newTask.getId());
             return new ResponseEntity<>(newTask, HttpStatus.CREATED);
         }catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PutMapping("/{id}/assign/user/{userId}")
+    public ResponseEntity<Task> addTask(@PathVariable Long userId, @PathVariable Long id) {
+        try {
+            Optional<Task> task = taskService.getTaskById(id);
+            Optional<UserEntity> userAssigned = userRepository.findById(userId);
+            Task assignedTask = task.get();
+            assignedTask.setUserAssigned(userAssigned.get());
+            taskService.addTask(assignedTask);
+            System.out.println(assignedTask.getUserAssigned().getId());
+            return new ResponseEntity<>(assignedTask, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 
     @DeleteMapping("/delete")
     public ResponseEntity<HttpStatus> deleteAllTasks() {
