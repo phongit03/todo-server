44ffd3eb57b835d215486e34b641a0f58c17a63f
phongit03 <phongtq003@gmail.com>
2024-06-20 22:05:16 +0700

    Added logger to TaskController for better debugging!

--- todo-server/src/main/java/com/todoapp/todo_server/TodoServerApplication.java
+++ todo-server/src/main/java/com/todoapp/todo_server/TodoServerApplication.java
@@ -1,11 +1,11 @@
 package com.todoapp.todo_server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoServerApplication.class, args);
 	}
--- todo-server/src/main/java/com/todoapp/todo_server/controller/TaskController.java
+++ todo-server/src/main/java/com/todoapp/todo_server/controller/TaskController.java
@@ -6,7 +6,9 @@ import com.todoapp.todo_server.entity.UserEntity;
 import com.todoapp.todo_server.repository.UserRepository;
 import com.todoapp.todo_server.service.TaskService;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +20,7 @@ import java.util.Optional;
 
 @RequestMapping(path = "/api/v1/tasks")
 @RestController
-
+@Log4j2
 public class TaskController {
     @Autowired
     public TaskService taskService;
@@ -29,12 +31,16 @@ public class TaskController {
     @GetMapping
     public ResponseEntity<List<Task>> getAllTasks() {
         try {
+            log.info("Fetching all tasks...");
             List<Task> tasks = taskService.getAllTasks();
             if(tasks.isEmpty()) {
+                log.warn("No List Task Found!");
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
+            log.info("{} Tasks Found!", tasks.size());
             return new ResponseEntity<>(tasks, HttpStatus.OK);
         }catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
@@ -43,12 +49,16 @@ public class TaskController {
     @GetMapping("/search/{title}")
     public ResponseEntity<List<Task>> searchTasksByName(@PathVariable String title) {
         try {
+            log.info("Fetching tasks by search value...");
             List<Task> tasks = taskService.getTasksByTitle(title);
             if(tasks.isEmpty()) {
+                log.warn("{} Tasks Found!", 0);
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
+            log.info("{} Tasks Found!", tasks.size());
             return new ResponseEntity<>(tasks, HttpStatus.OK);
         } catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -57,9 +67,12 @@ public class TaskController {
     @GetMapping("/user/{userId}")
     public ResponseEntity<Optional<List<Task>>> getTasksAssignedToUser(@PathVariable Long userId) {
         try {
+            log.info("Fetching tasks for user with id: {}...", userId);
             Optional<List<Task>> tasks = taskService.getTasksByUserId(userId);
+            tasks.ifPresent(taskList -> log.info("{} Tasks Found For User Id: {}", taskList.size(), userId));
             return new ResponseEntity<>(tasks, HttpStatus.OK);
         } catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -67,10 +80,13 @@ public class TaskController {
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         try {
+            log.info("Finding task with id: {}...", id);
             Optional<Task> taskData = taskService.getTaskById(id);
+            taskData.ifPresent(task -> log.info("Found task with id: {}!", id));
             return taskData.map(task -> new ResponseEntity<>(task, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
 
         }catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -78,13 +94,17 @@ public class TaskController {
     @PostMapping("/add/user/{userId}")
     public ResponseEntity<Task> addTask(@RequestBody Task taskRequest, @PathVariable Long userId) {
         try {
+            log.warn("Warning!, Only ADMIN role can perform this operation!");
+            log.info("Adding task and assigning to user with id: {}..." , userId);
             Task newTask = userRepository.findById(userId).map(user -> {
+               log.info("Assigning task to user...");
                taskRequest.setUserAssigned(user);
                return taskService.addTask(taskRequest);
             }).orElseThrow();
-            System.out.println(newTask.getId());
+            log.info("Added and assigned new task id: {} to user id: {} successfully!", newTask.getId(), userId);
             return new ResponseEntity<>(newTask, HttpStatus.CREATED);
         }catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -92,13 +112,16 @@ public class TaskController {
     @PutMapping("/{id}/assign/user/{userId}")
     public ResponseEntity<Task> addTask(@PathVariable Long userId, @PathVariable Long id) {
         try {
+            log.warn("Warning!, Only ADMIN role can perform this operation!");
+            log.info("Updating task id: {} assigning to user id: {}...", id, userId);
             Task assignedTask = taskService.getTaskById(id).get();
             UserEntity userAssigned = userRepository.findById(userId).get();
             assignedTask.setUserAssigned(userAssigned);
             taskService.addTask(assignedTask);
-            System.out.println(assignedTask.getUserAssigned().getId());
+            log.info("Updated task id: {} re-assigned to user id: {} successfully!", id, userId);
             return new ResponseEntity<>(assignedTask, HttpStatus.OK);
         }catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -106,11 +129,15 @@ public class TaskController {
     @PutMapping("/{id}/update/status/{status}")
     public ResponseEntity<Task> updateTaskStatus(@PathVariable Long id, @PathVariable String status) {
         try {
+
+            log.info("Updating status of task id: {}...", id);
             Task taskUpdate = taskService.getTaskById(id).get();
             taskUpdate.setStatus(status);
             taskService.addTask(taskUpdate);
+            log.info("Updated status: {} for task by id: {} successfully!", status, id);
             return new ResponseEntity<>(taskUpdate, HttpStatus.OK);
         } catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -119,9 +146,13 @@ public class TaskController {
     @DeleteMapping("/delete")
     public ResponseEntity<HttpStatus> deleteAllTasks() {
         try{
+            log.warn("Warning!, Only ADMIN role can perform this operation!");
+            log.info("Deleting all tasks...");
             taskService.deleteAllTasks();
+            log.info("Deleted all tasks successfully!!");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
@@ -130,9 +161,13 @@ public class TaskController {
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<HttpStatus> deleteTaskById(@PathVariable Long id) {
         try {
+            log.warn("Warning!, Only ADMIN role can perform this operation!");
+            log.info("Deleting task by id: {}...", id);
             taskService.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            log.info("Deleted task by id: {} successfully!", id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
+            log.error(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
